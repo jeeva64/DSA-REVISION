75f0825a992381112cd08d095a36d76eb5d71f1a
Jeeva Loganathan <169042323+jeeva64@users.noreply.github.com>
2026-01-08 20:51:55 +0530

    0136-SINGLE-NUMBER Stats: Time: 1 ms (99.86%), Space: 47.3 MB (6.4%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,19 +1,10 @@
 class Solution {
     public int singleNumber(int[] nums) {
-         
-        Map<Integer, Integer> map = new HashMap<>();
-
-        //Populate nums with frequency
-        for(int num : nums){
-            //If first time 1, else freq++
-            map.put(num, map.getOrDefault(num, 0) + 1);
+        int result = 0;
+    
+        for(int num : nums) {
+            result ^= num;
         }
-
-        //Check freq with 1 
-        for(int num : map.keySet()){
-            if(map.get(num) == 1)
-                return num;
-        }
-        return -1;
+        return result;
     }
 }
