8c8870164313683cf8f63db8d5709129d9abd8dc
Jeeva Loganathan <169042323+jeeva64@users.noreply.github.com>
2026-06-13 13:57:18 +0530

    Time: 2 ms (96.85%), Space: 46.6 MB (49.79%) - LeetHub

--- 3838-weighted-word-mapping/3838-weighted-word-mapping.java
+++ 3838-weighted-word-mapping/3838-weighted-word-mapping.java
@@ -1,24 +1,19 @@
 class Solution {
     public String mapWordWeights(String[] words, int[] weights) {
+        //Reversed Alphanbets
         char[] resChar = {'z', 'y', 'x', 'w', 'v', 'u', 't', 's', 'r', 'q', 'p', 'o', 'n', 'm', 'l', 'k', 'j', 'i', 'h', 'g', 'f', 'e', 'd', 'c', 'b', 'a'};
-        StringBuilder res = new StringBuilder();
-        
-        Map<Character, Integer> mapIdx = new HashMap<>();
-        int idx = 0;
-        for(char c = 'a'; c <= 'z'; c++) {
-            mapIdx.put(c, idx++);
-        }
+        StringBuilder sb = new StringBuilder();
 
         for(String word : words) {
-            int weight = 0;
-            for(char ch : word.toCharArray()) {
-                int index = mapIdx.get(ch);
-                weight += weights[index];
-            }
-            res.append(resChar[weight % 26]);
-            weight = 0;
+            int weightSum = 0;
+            //ASCII Index Conversion within 26
+            for(char ch : word.toCharArray()) 
+                weightSum += weights[ch - 'a'];
+
+            int idx = weightSum % 26;
+            sb.append(resChar[idx]);
         }
 
-        return res.toString();
+        return sb.toString();
     }
 }
