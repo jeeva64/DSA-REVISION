d19d60c9a4d14ed77881841bc895c4040219de1c
Jeeva Loganathan <169042323+jeeva64@users.noreply.github.com>
2026-07-03 23:33:45 +0530

    Time: 1 ms (99.89%), Space: 55.5 MB (59.4%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -3,9 +3,12 @@ class Solution {
         int current = 0, cnt = 0;
 
         for(int num : nums){
-            if(cnt == 0)          current = num;     //reset current = num, when cnt == 0
-            if(num == current)    cnt++;             //increase running frequency of num == current
-            if(num != current)    cnt--;             //decrease running frequency of num != current
+            if(cnt == 0)          
+                current = num;     //reset current = num, when cnt == 0
+            if(num == current)    
+                cnt++;             //increase running frequency of num == current
+            if(num != current)    
+                cnt--;             //decrease running frequency of num != current
         }
         return current;
     }
